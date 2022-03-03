0fe25eb22168b7dbb1901d4ede782f1ebdf8b537
SubSpecs <subspecs@gmail.com>
2022-03-04 00:53:10 +0200

    Upgrades ppl upgrades!

---
+++ Assets/Scripts/Backend.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Backend : MonoBehaviour
+{
+    public TMPro.TMP_Text TopAppTitle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Scene Managers/BaseSceneManager.cs
+++ Assets/Scripts/Scene Managers/BaseSceneManager.cs
@@ -6,6 +6,9 @@ public class BaseSceneManager : MonoBehaviour
     public TMPro.TMP_Text TopChannelTitle;
     public Image TopChannelTitleIcon;
 
+    public TMPro.TMP_Text TopServerTitle;
+    public Image TopServerTitleIcon;
+
     // Start is called before the first frame update
     void Start()
     {
